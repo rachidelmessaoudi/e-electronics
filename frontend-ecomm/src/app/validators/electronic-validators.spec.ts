b71433df1a957cb328b1ea162d5e042302cc0eae
El Messaoudi Rachid <Rachid.elmessaoudi97@gmail.com>
2020-12-22 09:46:32 +0100

    add validation

---
+++ frontend-ecomm/src/app/validators/electronic-validators.spec.ts
@@ -0,0 +1,7 @@
+import { ElectronicValidators } from './electronic-validators';
+
+describe('ElectronicValidators', () => {
+  it('should create an instance', () => {
+    expect(new ElectronicValidators()).toBeTruthy();
+  });
+});
